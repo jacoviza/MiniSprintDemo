9639e74b10a3bd8e0749c6744dfcc0b83ac310bb
Jacobo Villarreal <jvillarreal@neural.com.do>
2022-08-08 23:51:31 -0400

    Initial commit

---
+++ MiniSprintApp/User.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniSprintApp
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
---
+++ MiniSprintSchema/Tables/users.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [Username] VARCHAR(100) NULL, 
+    [Password] VARCHAR(100) NULL
+)
